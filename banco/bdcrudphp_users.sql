64d705ae859cbdad8cf67bc965a9a148348fe8ab
omarcus-vinnicius <marquinhosdosonhos@gmail.com>
2024-02-28 17:29:17 -0300

    completo

--- banco/bdcrudphp_users.sql
+++ banco/bdcrudphp_users.sql
@@ -20,7 +20,9 @@ USE `bdcrudphp`;
 --
 -- Table structure for table `users`
 --
+ 
 
+ 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
